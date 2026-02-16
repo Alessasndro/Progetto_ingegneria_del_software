a793793d73a1cb3a3098734c0ef9c58bcc0e266d
alessasndro <valentin.enache@edu.unifi.it>
2026-02-16 12:15:58 +0100

    connessione db con main temporaneo di gemini che ci avverte se la connessione e riuscita

--- src/main/java/com/ads/db/DatabaseConnection.java
+++ src/main/java/com/ads/db/DatabaseConnection.java
@@ -4,8 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/American_dream_simulator";
-    private static final String USER = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/American_dream_simulator?createDatabaseIfNotExist=true";    private static final String USER = "root";
     private static final String PASSWORD = "1234";
 
     public static Connection getConnection() throws SQLException {
